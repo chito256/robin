f6df3bed4211d0fbea52decada4b14ad0732c5db
Chi To <williamto256@gmail.com>
2016-11-22 08:57:33 +0700

    Add FriendList

---
+++ src/main/resources/static/assets/js/components/layout/LayoutSwitcher/FriendList.js
@@ -0,0 +1,28 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const FriendList = React.createClass({
+  render() {
+    return (
+      <div className="friend-list">
+        <ul>
+          <li>Friend 1</li>
+          <li>Friend 2</li>
+          <li>Friend 3</li>
+          <li>Friend 3</li>
+          <li>Friend 3</li>
+          <li>Friend 3</li>
+          <li>Friend 3</li>
+          <li>Friend 3</li>
+          <li>Friend 3</li>
+          <li>Friend 3</li>
+          <li>Friend 3</li>
+          <li>Friend 3</li>
+          <li>Friend 3</li>
+        </ul>
+      </div>
+    )
+  }
+});
+
+module.exports = FriendList;
--- src/main/resources/static/assets/js/components/layout/LayoutSwitcher/LayoutSwitcher.js
+++ src/main/resources/static/assets/js/components/layout/LayoutSwitcher/LayoutSwitcher.js
@@ -2,12 +2,14 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 
 const Writer = require('./Writer');
+const FriendList = require('./FriendList');
 
 const LayoutSwitcher = React.createClass({
   render() {
     return (
       <div className="layout-switcher">
         <Writer />
+        {/* <FriendList /> */}
       </div>
     )
   }
