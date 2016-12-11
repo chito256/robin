8c0167474de55f7330a2b7251a863e813053bbb2
Chi To <williamto256@gmail.com>
2016-12-11 21:14:56 +0700

    Add friend list

--- src/main/resources/static/assets/js/components/layout/layoutswitcher/friendlist/FriendList.js
+++ src/main/resources/static/assets/js/components/layout/layoutswitcher/friendlist/FriendList.js
@@ -1,27 +1,43 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+const Friend = require('./Friend');
+
+// Mock data
+const friends = [
+  {
+    id: 0,
+    name: 'Ben J. Lee',
+    role: 'Software Engineer at Spotify',
+    avatar: 'https://www.fillmurray.com/64/64'
+  },
+  {
+    id: 1,
+    name: 'Cameron Switzer',
+    role: 'Designer at Spotify',
+    avatar: 'https://www.fillmurray.com/64/64'
+  },
+  {
+    id: 2,
+    name: 'Layla McCabe',
+    role: 'Head of Marketing at YouTube',
+    avatar: 'https://www.fillmurray.com/64/64'
+  },
+];
+
 const FriendList = React.createClass({
   render() {
     return (
       <div className="friend-list friendlist-inactive">
         <ul>
-          <li>Friend 1</li>
-          <li>Friend 2</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
-          <li>Friend 3</li>
+          {
+            friends.map((friend) => {
+              return <Friend  key={friend.id}
+                              name={friend.name}
+                              role={friend.role}
+                              avatar={friend.avatar} ></Friend>
+            })
+          }
         </ul>
       </div>
     )
