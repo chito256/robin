b19e63d450c07ef46243ee122f1ff6323d0a66cc
Chi To <williamto256@gmail.com>
2016-12-26 10:15:03 +0700

    Remove Webpack

--- src/main/resources/gulpfile.js
+++ src/main/resources/gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var webpack = require('gulp-webpack');
 
 gulp.task('sass', function () {
   return gulp.src('./static/assets/css/main.scss')
@@ -8,15 +7,8 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./static/build/'));
 });
 
-gulp.task('bundle', function () {
-  return gulp.src('./static/assets/js/index.js')
-        .pipe(webpack(require('./webpack.config.js')))
-        .pipe(gulp.dest('./static/build/'));
-});
-
 gulp.task('watch', function () {
   gulp.watch('./static/assets/css/**/*.scss', ['sass']);
-  gulp.watch('./static/assets/js/**/**/**/*.js', ['bundle']);
 });
 
-gulp.task('default', ['bundle', 'sass', 'watch']);
+gulp.task('default', ['sass', 'watch']);
